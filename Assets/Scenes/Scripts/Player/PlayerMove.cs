6cbe29b5eac5d11158241f9120e552fac373fb24
MellyMadame <62388904+MellyMadame@users.noreply.github.com>
2023-12-20 10:42:40 +0100

    current status
    Player is able to go right and left

---
+++ Assets/Scenes/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 3;
+
+    // Die Geschwindigkeit, in der nach rechts oder links gelaufen wird
+    public float leftRightSpeed = 4;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector 3 = z-Achse, 
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) { 
+            if(this.gameObject.transform.position.x > LevelBoundary.leftSide) 
+            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+        }
+         if (Input.GetKey(KeyCode.RightArrow)) {  
+            // die Minus 1 zum Schluss invertiert die Bewegung nach links. Somit geht der Spieler nach rechts. 
+            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
+        }
+    }
+}
