cd787f700f3a4d2c78a87a138d8940dfd484e1a0
MellyMadame <62388904+MellyMadame@users.noreply.github.com>
2023-12-20 14:53:20 +0100

    colliding does not work
    nearly

--- Assets/Scenes/Scripts/Player/PlayerMove.cs
+++ Assets/Scenes/Scripts/Player/PlayerMove.cs
@@ -9,14 +9,14 @@ public class PlayerMove : MonoBehaviour
     // Die Geschwindigkeit, in der nach rechts oder links gelaufen wird
     public float leftRightSpeed = 4;
     //es muss überprüft werden, ob der Spieler sich bewegen kann. Für den Start des Spiels (Tutorial 12)
-   // static public bool canMove = false;
+    static public bool canMove = true;
    
     // Update is called once per frame
     void Update()
     {
         //Vector 3 = z-Achse, 
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-        //if (canMove == true){
+        if (canMove == true){
             if (Input.GetKey(KeyCode.LeftArrow)) { 
             if(this.gameObject.transform.position.x > LevelBoundary.leftSide) 
             transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
@@ -25,6 +25,6 @@ public class PlayerMove : MonoBehaviour
             // die Minus 1 zum Schluss invertiert die Bewegung nach links. Somit geht der Spieler nach rechts. 
             transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed * -1);
              }
-        //}
+        }
     }
 }
