69bb738fa3a7f4bcb896a72e2fca729a95b8740c
MellyMadame <62388904+MellyMadame@users.noreply.github.com>
2024-01-06 12:24:51 +0100

    Player jumps
    Der Spieler kann jetzt springen

--- Assets/Scenes/Scripts/Player/PlayerMove.cs
+++ Assets/Scenes/Scripts/Player/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -11,10 +12,17 @@ public class PlayerMove : MonoBehaviour
     //es muss überprüft werden, ob der Spieler sich bewegen kann. Für den Start des Spiels (Tutorial 12)
     static public bool canMove = false;
     private bool isOnLeftSide = true; // Starting on the left side
-   
+    
+    public bool isJumping = false;
+    public bool comingDown = false;
+    
+    public GameObject playerObject;
+
     // Update is called once per frame
     void Update()
     {
+         bool jumpInput = Input.GetButtonDown("Jump");
+
         //Vector 3 = z-Achse, 
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
         if (canMove == true){
@@ -25,15 +33,45 @@ public class PlayerMove : MonoBehaviour
             // Moves an object up 2 units
             transform.position += new Vector3(4, 0, 0);
         }
-        else if (!isOnLeftSide && Input.GetKey(KeyCode.LeftArrow))
+        if (!isOnLeftSide && Input.GetKey(KeyCode.LeftArrow))
         {   
             transform.position += new Vector3(-4, 0, 0);
             SwitchSide();
+        }
+            if(Input.GetKey(KeyCode.UpArrow)){
+                if(isJumping == false)
+                {
+                    isJumping = true;
+                    playerObject.GetComponent<Animator>().Play("mixamo_com");
+                    StartCoroutine(JumpSequence());
+                }
+            }
+      
+
+        // Handle jumping
+        if (isJumping == true)
+        {
+            if(comingDown == false){
+                transform.Translate(Vector3.up * Time.deltaTime * 3, Space.World);
+            }
+            if(comingDown == true){
+                transform.Translate(Vector3.up * Time.deltaTime * -3, Space.World);
+            }
         }
         }
     }
      void SwitchSide() {
         isOnLeftSide = !isOnLeftSide;
+    }
+    
+    IEnumerator JumpSequence(){
+        yield return new WaitForSeconds(0.45f);
+        comingDown = true;
+        yield return new WaitForSeconds(0.45f);
+        isJumping = false;
+        comingDown = false;
+        playerObject.GetComponent<Animator>().Play("Standard Run");
+
     }
     
 }
