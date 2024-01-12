e7e482c4b3a1a7035cf64ec0cb4d7c9a3884841a
Eddi <73501429+WallWard@users.noreply.github.com>
2024-01-12 17:20:34 +0100

    RetryWindow
    Ein Fenster eingefügt, welches das Spiel nach erreichen von 0 Leben resettet oder das Spiel beendet.

--- Assets/Scenes/Scripts/Player/PlayerMove.cs
+++ Assets/Scenes/Scripts/Player/PlayerMove.cs
@@ -12,10 +12,10 @@ public class PlayerMove : MonoBehaviour
     //es muss überprüft werden, ob der Spieler sich bewegen kann. Für den Start des Spiels (Tutorial 12)
     static public bool canMove = false;
     private bool isOnLeftSide = true; // Starting on the left side
-    
+
     public bool isJumping = false;
     public bool comingDown = false;
-    
+
     public GameObject playerObject;
 
     // Update is called once per frame
@@ -24,46 +24,52 @@ public class PlayerMove : MonoBehaviour
 
         //Vector 3 = z-Achse, 
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
-        if (canMove == true){
-
-             if (isOnLeftSide && Input.GetKey(KeyCode.RightArrow))
+        if (canMove == true)
         {
-            SwitchSide();
-            // Moves an object up 2 units
-            transform.position += new Vector3(4, 0, 0);
-        }
-        if (!isOnLeftSide && Input.GetKey(KeyCode.LeftArrow))
-        {   
-            transform.position += new Vector3(-4, 0, 0);
-            SwitchSide();
-        }
-            if(Input.GetKey(KeyCode.UpArrow)){
-                if(isJumping == false)
+
+            if (isOnLeftSide && Input.GetKey(KeyCode.RightArrow))
+            {
+                SwitchSide();
+                // Moves an object up 2 units
+                transform.position += new Vector3(4, 0, 0);
+            }
+            if (!isOnLeftSide && Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-4, 0, 0);
+                SwitchSide();
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                if (isJumping == false)
                 {
                     isJumping = true;
                     playerObject.GetComponent<Animator>().Play("Jumping Up");
                     StartCoroutine(JumpSequence());
                 }
             }
-      
 
-        // Handle jumping
-        if (isJumping == true)
-        {
-            if(comingDown == false){
-                transform.Translate(Vector3.up * Time.deltaTime * 3, Space.World);
+
+            // Handle jumping
+            if (isJumping == true)
+            {
+                if (comingDown == false)
+                {
+                    transform.Translate(Vector3.up * Time.deltaTime * 3, Space.World);
+                }
+                if (comingDown == true)
+                {
+                    transform.Translate(Vector3.up * Time.deltaTime * -3, Space.World);
+                }
             }
-            if(comingDown == true){
-                transform.Translate(Vector3.up * Time.deltaTime * -3, Space.World);
-            }
-        }
         }
     }
-     void SwitchSide() {
+    void SwitchSide()
+    {
         isOnLeftSide = !isOnLeftSide;
     }
-    
-    IEnumerator JumpSequence(){
+
+    IEnumerator JumpSequence()
+    {
         yield return new WaitForSeconds(0.45f);
         comingDown = true;
         yield return new WaitForSeconds(0.45f);
@@ -72,5 +78,5 @@ public class PlayerMove : MonoBehaviour
         playerObject.GetComponent<Animator>().Play("Standard Run");
 
     }
-    
+
 }
